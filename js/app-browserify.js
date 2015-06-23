cd315541eecb7e53b4579673b4ddd13efdc82b7a
Matthew Keas <matthiasak@gmail.com>
2015-06-23 15:28:53 -0500

    first push, pubnub connected

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -5,22 +5,72 @@ require("es5-shim")
 // es6 polyfills, powered by babel
 require("babel/register")
 
-var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+import {Promise} from 'es6-promise'
+import React from 'react'
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+const subscribe_key = `sub-c-6b8a95c8-19ab-11e5-bc40-02ee2ddab7fe`,
+    publish_key  = `pub-c-cb375025-8aee-4e13-9626-087026c2677d`
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+const pubnub = PUBNUB({                         
+    publish_key   : publish_key,
+    subscribe_key : subscribe_key
+})
 
-// window.addEventListener('load', app)
+const subscribe = (channel, message) => pubnub.subscribe({ channel, message })
 
-// function app() {
-    // start app
-    // new Router()
-// }
+const publish = (channel, message, callback) => pubnub.publish({ channel, message, callback })
 
+// window.app = {pubnub, subscribe, publish}
+
+// setInterval(() => publish('hello', {name: 'matt'}), 5000)
+// publish('hello', 'test message')
+// subscribe('hello', (data) => console.log(data))
+
+var name = window.prompt('What\'s your name?')
+// var messages = [] //--> { sender: 'Paul', text: 'Hey, baby. You're new here, aren't you? }
+
+class Message extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (<li>
+            <p>{this.props.message.text}</p>
+            <span>{this.props.message.name}</span>
+        </li>)
+    }
+}
+
+class Chat extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            messages: []
+        }
+    }
+    componentWillMount(){
+        subscribe('meeting_room', (data) => {
+            var newMessages = this.state.messages.concat(data)
+            this.setState({messages: newMessages})
+        })
+    }
+    _send(e){
+        e.preventDefault()
+        var text = React.findDOMNode(this.refs.message)
+        publish('meeting_room', {name, text: text.value})
+        text.value = ''
+    }
+    render(){
+        return (<div>
+            <ul>
+                {this.state.messages.map((data) => <Message message={data} />)}
+            </ul>
+            <form onSubmit={(e) => this._send(e)}>
+                <div><input ref="message" /></div>
+                <button>send</button>
+            </form>
+        </div>)
+    }
+}
+
+React.render(<Chat />, document.querySelector('.container'))
